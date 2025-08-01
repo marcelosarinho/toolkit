4e73648f6c1201ded3c3f7420654bb650fcda1f6
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-01 17:50:36 -0300

    feat: get days between function (incomplete)

---
+++ src/date/getDaysBetween.ts
@@ -0,0 +1,29 @@
+import { toMidnightUTC } from "./utils";
+import { isValidDate } from "./validate";
+
+/**
+ * Calculates the number of days between two dates.
+ * @param {Date} date1 - The start date.
+ * @param {Date} date2 - The end date.
+ * @returns {number | undefined} The number of days between the two dates, or undefined if the input is invalid.
+ */
+export default function getDaysBetween(date1: Date, date2: Date): number | undefined {
+  if (!isValidDate(date1)) {
+    console.log(`Date ${date1} is invalid! Please provide a date in these formats: YYYY-MM-DDTHH:mm:ss.sssZ, YYYY, YYYY-MM, YYYY-MM-DD`);
+    return;
+  }
+
+  if (!isValidDate(date2)) {
+    console.log(`Date ${date2} is invalid! Please provide a date in these formats: YYYY-MM-DDTHH:mm:ss.sssZ, YYYY, YYYY-MM, YYYY-MM-DD`);
+    return;
+  }
+
+  date1 = toMidnightUTC(date1);
+  date2 = toMidnightUTC(date2);
+  const result = Math.abs(date1.getTime() - date2.getTime());
+  const days = result / 60 / 60 / 24 / 1000;
+
+  return days;
+}
+
+console.log(getDaysBetween(new Date(), new Date('2025-08-06')));
