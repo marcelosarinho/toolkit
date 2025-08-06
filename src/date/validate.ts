a46f508eb39f4b1bd1fa86e1c86116d1e4c51a52
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-06 14:47:09 -0300

    feat: isPast validation

--- src/date/validate.ts
+++ src/date/validate.ts
@@ -1,19 +1,27 @@
 import * as C from "./constants";
 
-export function isValidDate(date: Date) {
+export function isValidDate(date: Date): boolean {
   return !(date.toString() === C.INVALID_DATE_STRING);
 }
 
-export function isInteger(number: number) {
+export function isInteger(number: number): boolean {
   return Number.isInteger(number);
 }
 
-export function isLessThanOne(number: number) {
+export function isLessThanOne(number: number): boolean {
   return number < 1
 }
 
-export function isWeekend(date: Date) {
+export function isWeekend(date: Date): boolean {
   if (date.getDay() === 0 || date.getDay() === 6) {
+    return true;
+  }
+
+  return false;
+}
+
+export function isPast(date: Date) {
+  if (date < new Date()) {
     return true;
   }
 
