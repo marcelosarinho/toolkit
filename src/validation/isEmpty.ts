c7f8b91bb8265d4574648aea1379cfdd69d2bd55
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-06 18:40:03 -0300

    feat: isEmpty documentation

--- src/validation/isEmpty.ts
+++ src/validation/isEmpty.ts
@@ -1,7 +1,28 @@
 /**
- * Verify if a value is empty or not
- * @param {any} value - Any value
- * @returns {boolean} Returns `true` if the value is empty, or `false` if the value isn't empty
+ * Checks if a given value is empty.
+ *
+ * This function returns `true` if the value is:
+ * - `null`
+ * - `undefined`
+ * - `false`
+ * - `0`
+ * - An empty string `""`
+ * - `NaN`
+ *
+ * Otherwise, it returns `false`.
+ *
+ * @param {any} value - The value to check for emptiness.
+ * @returns {boolean} Returns `true` if the value is considered empty, otherwise `false`.
+ *
+ * @example
+ * isEmpty(null);      // true
+ * isEmpty(undefined); // true
+ * isEmpty(false);     // true
+ * isEmpty(0);         // true
+ * isEmpty('');        // true
+ * isEmpty(NaN);       // true
+ * isEmpty('hello');   // false
+ * isEmpty(123);       // false
  */
 export default function isEmpty(value: any): boolean {
   if (!value) {
