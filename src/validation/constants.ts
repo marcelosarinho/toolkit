69859eb2fdfeea00eb7eef735847f234a83f8e06
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-07 10:01:46 -0300

    feat: uuidv4 regex const

--- src/validation/constants.ts
+++ src/validation/constants.ts
@@ -1,3 +1,3 @@
 export const SIMPLE_EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 export const COMPLEX_EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const UUID_V4_REGEX = /^()-()-()-()$/;
+export const UUID_V4_REGEX = /^([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})$/;
