bf13ae0c7c0c5a4b6646fea74899f1fe04bd3610
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-07 10:02:49 -0300

    feat: isUUIDv4 function

--- src/validation/isUUIDv4.ts
+++ src/validation/isUUIDv4.ts
@@ -1,5 +1,5 @@
-export default function isUUIDv4(value: string): boolean {
-  
-  
-  return false;
+import * as C from "./constants";
+
+export default function isUUIDv4(value: string): boolean {  
+  return C.UUID_V4_REGEX.test(value);
 }
