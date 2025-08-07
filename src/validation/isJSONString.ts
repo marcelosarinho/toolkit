7f8e32aacde222fbf0e15bb79c356cb45c40e7ea
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-07 10:25:01 -0300

    feat: isJSONString and isUUIDv4 documentation

--- src/validation/isJSONString.ts
+++ src/validation/isJSONString.ts
@@ -1,4 +1,9 @@
-export default function isJSONString(value: string) {
+/**
+ * Verify if a string is a valid JSON
+ * @param {string} value - The string to be parsed as JSON
+ * @returns {boolean} Returns `true` if the string is a valid JSON, or `false` if it isn't
+ */
+export default function isJSONString(value: string): boolean {
   try {
     JSON.parse(value);
 
--- src/validation/isUUIDv4.ts
+++ src/validation/isUUIDv4.ts
@@ -1,5 +1,10 @@
 import * as C from "./constants";
 
+/**
+ * Verify if a string is UUIDv4
+ * @param {string} value - The string to be verified
+ * @returns {boolean} Returns `true` if the string is UUIDv4, or `false` if it isn't
+ */
 export default function isUUIDv4(value: string): boolean {  
   return C.UUID_V4_REGEX.test(value);
 }
